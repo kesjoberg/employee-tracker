2bd27a7dad26626a6d00146e06fdfc783e40ea74
Karna Sjoberg <kesjoberg@gmail.com>
2021-10-26 10:00:55 -0500

    updates to seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,12 +9,14 @@ VALUES
 
   INSERT INTO role(title, salary, department_id)
   VALUES 
-    ("Lead Engineer", 100000, 1),
+    ("Lead Engineer", 120000, 1),
     ("Accountant Manager", 90000, 2),
     ("Legal Team Lead", 95000, 3),
     ("Sales Manager", 85000,4),
     ("Jr Developer", 50000, 1),
-    ("Lawyer", 85000,3);
+    ("Lawyer", 85000,3)
+    ("Paralegal", 42000, 3)
+    ("Scrum Master", 90000, 1);
 
   INSERT INTO employee(first_name, last_name, role_id, manager_id)
   VALUES
@@ -23,4 +25,5 @@ VALUES
     ("Sally", "Jones", 3, NULL),
     ("Jolene", "Marks", 4, NULL),
     ("Harry", "James", 5, 1),
-    ("Ellen", "Johnson", 1, 3);
+    ("Ellen", "Johnson", 8, 1)
+    ("Kristen", "Carson", 7, 3);
